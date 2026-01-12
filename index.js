b0c1757b292453acd9b9862a97325b1f5bb5d764
rahat <57500825+Coder8128@users.noreply.github.com>
2026-01-12 20:02:39 +0100

    Implement game logic start

---
+++ index.js
@@ -0,0 +1,32 @@
+function boardCreate() {
+    let gameBoard = [];
+    let x = 0;
+
+    for (let i = 0; i < 3; i++) {
+        gameBoard[i] = [];
+        for (let j = 0; j < 3; j++) {
+            gameBoard[i][j] = x;
+        }
+    }
+
+    const getBoard = () => gameBoard;
+
+    const handleMove = (row, col) => {
+        gameBoard[row][col] = 1;
+        console.log(gameBoard);
+    }
+
+
+    return { getBoard, handleMove };
+}
+
+//boardCreate();
+
+function startGame() {
+    let board = boardCreate();
+    console.log(board.getBoard());
+
+
+
+}
+
